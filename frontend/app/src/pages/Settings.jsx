5ef6dbe6ae7c359dbd1e7cf87bd7f0c1ce38937a
mckimssongg <86619053+mckimssongg@users.noreply.github.com>
2022-05-11 17:08:37 -0600

    add

--- frontend/app/src/pages/Settings.jsx
+++ frontend/app/src/pages/Settings.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Modal } from "../modal/index";
-import { ContextGlobal } from "../context/Context";
+// import { ContextGlobal } from "../context/Context";
 import Estacionamiento from "../assets/img/estacionamiento.png";
 import roles from "../assets/img/roles.png";
 import usuarios from "../assets/img/verificar.png";
@@ -9,7 +9,9 @@ import usuarios from "../assets/img/verificar.png";
 function Settings() {
   const navigate = useNavigate();
 
-  const { setOpenModal, openModal } = React.useContext(ContextGlobal);
+  const [openModal, setOpenModal] = React.useState(false);
+  const [openModal2, setOpenModal2] = React.useState(false);
+  const [openModal3, setOpenModal3] = React.useState(false);
 
   React.useEffect(() => {
     if (!localStorage.getItem("dataSesion")) {
@@ -27,17 +29,47 @@ function Settings() {
     <div className="container">
       <h3 className="text-center mt-2">Configuración</h3>
       <div className="row text-center justify-content-evenly align-items-center mt-5">
-        <div className="col col-lg-2  HoverStandar p-4">
+        <div
+          className="col col-lg-2  HoverStandar p-4"
+          onClick={() => {
+            setOpenModal((prevState) => !prevState);
+          }}
+        >
           Registrar usuarios
           <img src={usuarios} className="img-fluid" />
+          {!!openModal && (
+            <Modal>
+              <button onClick={() => {}}>Regresar</button>
+            </Modal>
+          )}
         </div>
-        <div className="col col-lg-2   HoverStandar  p-4">
+        <div
+          className="col col-lg-2   HoverStandar  p-4"
+          onClick={() => {
+            setOpenModal2((prevState) => !prevState);
+          }}
+        >
           Administrar Roles
           <img src={roles} className="img-fluid" />
+          {!!openModal2 && (
+            <Modal>
+              <button onClick={() => {}}>Regresar2</button>
+            </Modal>
+          )}
         </div>
-        <div className="col-7 col-sm-4 col-lg-2  HoverStandar p-4">
+        <div
+          className="col-7 col-sm-4 col-lg-2  HoverStandar p-4"
+          onClick={() => {
+            setOpenModal3((prevState) => !prevState);
+          }}
+        >
           Administrar Estacionamientos y areas
           <img src={Estacionamiento} className="img-fluid" />
+          {!!openModal3 && (
+            <Modal>
+              <button onClick={() => {}}>Regresar3</button>
+            </Modal>
+          )}
         </div>
       </div>
     </div>
